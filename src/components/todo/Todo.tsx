b3da60c96c773919aca1f650c3bcfa8151c57ddf
dammarin <marinatosd@gmail.com>
2025-12-23 19:49:36 +0200

    feat(todos): implement TodoList logic to Todo component

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -16,6 +16,22 @@ const Todo = () => {
     console.log(todos);
   }
 
+  const deleteTodo = (id: number) => {
+    setTodos(prev => prev.filter(todo => todo.id !== id));
+  }
+
+  const editTodo = (id: number, newText: string) => {
+    setTodos(prev => prev.map(todo => todo.id === id ? {...todo, text: newText} : todo))
+  }
+
+  const toggleTodo = (id: number) => {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    )
+  }
+
   useEffect(() => {
     inputRef.current?.focus();
   }, [todos]);
@@ -25,7 +41,7 @@ const Todo = () => {
       <div className="max-w-sm mx-auto">
         <h1 className="text-center text-2xl py-8">To-Do List</h1>
         <TodoForm addTodo={addTodo} inputRef={inputRef} />
-        <TodoList todos={todos} />
+        <TodoList todos={todos} editTodo={editTodo} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
       </div>
     </>
   )
